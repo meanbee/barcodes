5898d0044b7d2cdf76068302b534b73a38479683
Ashley Wright <ashley.wright@meanbee.com>
2016-06-15 16:17:40 +0100

    Add barcodes to Invoice Numbers, Shipment Numbers, Credit Meno Numbers and Product SKU codes.

--- app/code/community/Meanbee/BarcodeAnnotator/Helper/Config.php
+++ app/code/community/Meanbee/BarcodeAnnotator/Helper/Config.php
@@ -2,6 +2,10 @@
 class Meanbee_BarcodeAnnotator_Helper_Config extends Mage_Core_Helper_Abstract {
     const XML_ENABLED = 'meanbee_barcodeannotator/general/active';
     const XML_ORDER_NUMBER_BARCODE_SYMBOLOGY = 'meanbee_barcodeannotator/general/order_number_barcode_symbology';
+    const XML_INVOICE_NUMBER_BARCODE_SYMBOLOGY = 'meanbee_barcodeannotator/general/invoice_number_barcode_symbology';
+    const XML_SHIPMENT_NUMBER_BARCODE_SYMBOLOGY = 'meanbee_barcodeannotator/general/shipment_number_barcode_symbology';
+    const XML_CREDITMEMO_NUMBER_BARCODE_SYMBOLOGY = 'meanbee_barcodeannotator/general/creditmemo_number_barcode_symbology';
+    const XML_PRODUCT_SKU_BARCODE_SYMBOLOGY = 'meanbee_barcodeannotator/general/product_sku_barcode_symbology';
 
     /**
      * @param null $store
@@ -19,5 +23,41 @@ class Meanbee_BarcodeAnnotator_Helper_Config extends Mage_Core_Helper_Abstract {
      */
     public function getOrderNumberBarcodeSymbology($store = null) {
         return Mage::getStoreConfig(self::XML_ORDER_NUMBER_BARCODE_SYMBOLOGY, $store);
+    }
+
+    /**
+     * @param null $store
+     *
+     * @return string
+     */
+    public function getInvoiceNumberBarcodeSymbology($store = null) {
+        return Mage::getStoreConfig(self::XML_INVOICE_NUMBER_BARCODE_SYMBOLOGY, $store);
+    }
+
+    /**
+     * @param null $store
+     *
+     * @return string
+     */
+    public function getShipmentNumberBarcodeSymbology($store = null) {
+        return Mage::getStoreConfig(self::XML_SHIPMENT_NUMBER_BARCODE_SYMBOLOGY, $store);
+    }
+
+    /**
+     * @param null $store
+     *
+     * @return string
+     */
+    public function getCreditMemoNumberBarcodeSymbology($store = null) {
+        return Mage::getStoreConfig(self::XML_CREDITMEMO_NUMBER_BARCODE_SYMBOLOGY, $store);
+    }
+    
+    /**
+     * @param null $store
+     *
+     * @return string
+     */
+    public function getProductSkuBarcodeSymbology($store = null) {
+        return Mage::getStoreConfig(self::XML_PRODUCT_SKU_BARCODE_SYMBOLOGY, $store);
     }
 }
--- app/code/community/Meanbee/BarcodeAnnotator/Model/Observer.php
+++ app/code/community/Meanbee/BarcodeAnnotator/Model/Observer.php
@@ -11,24 +11,101 @@ class Meanbee_BarcodeAnnotator_Model_Observer
     public function updateSalesGridColumns(Varien_Event_Observer $observer)
     {
         $config = Mage::helper('meanbee_barcodeannotator/config');
-        if (Mage::app()->getRequest()->getControllerName() != "sales_order" || !$config->getEnabled()) {
+        if (!$config->getEnabled()) {
             return;
         }
 
         $block = $observer->getEvent()->getBlock();
-        if ($block instanceof Mage_Adminhtml_Block_Widget_Grid_Massaction_Abstract) {
-            /** @var Mage_Adminhtml_Block_Sales_Order_Grid $block */
-            $block = $block->getLayout()->getBlock("sales_order.grid");
-            if (!$block) {
+        switch (Mage::app()->getRequest()->getControllerName()) {
+            case 'sales_order':
+                if ($block instanceof Mage_Adminhtml_Block_Widget_Grid_Massaction_Abstract) {
+                    /** @var Mage_Adminhtml_Block_Sales_Order_Grid $block */
+                    $block = $block->getLayout()->getBlock('sales_order.grid');
+                    if (!$block) {
+                        return;
+                    }
+                    if (($column = $block->getColumn('real_order_id')) && $config->getOrderNumberBarcodeSymbology()) {
+                        $column->setFrameCallback(array($this, 'decorateOrderNumber'));
+                    }
+                }
                 return;
-            }
-            if (($column = $block->getColumn("real_order_id")) && $config->getOrderNumberBarcodeSymbology()) {
-                $column->setFrameCallback(array($this, 'decorateOrderNumber'));
-            }
+            case 'sales_invoice':
+                if ($block instanceof Mage_Adminhtml_Block_Widget_Grid_Massaction_Abstract) {
+                    /** @var Mage_Adminhtml_Block_Sales_Invoice_Grid $block */
+                    $block = $block->getLayout()->getBlock('sales_invoice.grid');
+                    if (!$block) {
+                        return;
+                    }
+                    if (($column = $block->getColumn('increment_id')) && $config->getInvoiceNumberBarcodeSymbology()) {
+                        $column->setFrameCallback(array($this, 'decorateInvoiceNumber'));
+                    }
+                    if (($column = $block->getColumn('order_increment_id')) && $config->getOrderNumberBarcodeSymbology()) {
+                        $column->setFrameCallback(array($this, 'decorateOrderNumber'));
+                    }
+                }
+                return;
+            case 'sales_shipment':
+                if ($block instanceof Mage_Adminhtml_Block_Widget_Grid_Massaction_Abstract) {
+                    /** @var Mage_Adminhtml_Block_Sales_Shipment_Grid $block */
+                    $block = $block->getLayout()->getBlock('sales_shipment.grid');
+                    if (!$block) {
+                        return;
+                    }
+                    if (($column = $block->getColumn('increment_id')) && $config->getShipmentNumberBarcodeSymbology()) {
+                        $column->setFrameCallback(array($this, 'decorateShipmentNumber'));
+                    }
+                    if (($column = $block->getColumn('order_increment_id')) && $config->getOrderNumberBarcodeSymbology()) {
+                        $column->setFrameCallback(array($this, 'decorateOrderNumber'));
+                    }
+                }
+                return;
+            case 'sales_creditmemo':
+                if ($block instanceof Mage_Adminhtml_Block_Widget_Grid_Massaction_Abstract) {
+                    /** @var Mage_Adminhtml_Block_Sales_Creditmemo_Grid $block */
+                    $block = $block->getLayout()->getBlock('sales_creditmemo.grid');
+                    if (!$block) {
+                        return;
+                    }
+                    if (($column = $block->getColumn('increment_id')) && $config->getCreditMemoNumberBarcodeSymbology()) {
+                        $column->setFrameCallback(array($this, 'decorateCreditMemoNumber'));
+                    }
+                    if (($column = $block->getColumn('order_increment_id')) && $config->getOrderNumberBarcodeSymbology()) {
+                        $column->setFrameCallback(array($this, 'decorateOrderNumber'));
+                    }
+                }
+                return;
+            case 'catalog_product':
+                if ($block instanceof Mage_Adminhtml_Block_Widget_Grid_Massaction_Abstract) {
+                    /** @var Mage_Adminhtml_Block_Catalog_Product_Grid $block */
+                    $block = $block->getLayout()->getBlock('product.grid');
+                    if (!$block) {
+                        return;
+                    }
+                    if (($column = $block->getColumn('sku')) && $config->getProductSkuBarcodeSymbology()) {
+                        $column->setFrameCallback(array($this, 'decorateProductSku'));
+                    }
+                }
+                return;
         }
     }
-
+    
     public function decorateOrderNumber($value, $row, $column, $isExport) {
         return sprintf('<div title class="barcode" data-barcode-symbology="%s">%s</div>', Mage::helper('meanbee_barcodeannotator/config')->getOrderNumberBarcodeSymbology(), $value);
+    }
+
+    public function decorateInvoiceNumber($value, $row, $column, $isExport) {
+        return sprintf('<div title class="barcode" data-barcode-symbology="%s">%s</div>', Mage::helper('meanbee_barcodeannotator/config')->getInvoiceNumberBarcodeSymbology(), $value);
+    }
+
+    public function decorateShipmentNumber($value, $row, $column, $isExport) {
+        return sprintf('<div title class="barcode" data-barcode-symbology="%s">%s</div>', Mage::helper('meanbee_barcodeannotator/config')->getShipmentNumberBarcodeSymbology(), $value);
+    }
+
+    public function decorateCreditMemoNumber($value, $row, $column, $isExport) {
+        return sprintf('<div title class="barcode" data-barcode-symbology="%s">%s</div>', Mage::helper('meanbee_barcodeannotator/config')->getCreditMemoNumberBarcodeSymbology(), $value);
+    }
+
+    public function decorateProductSku($value, $row, $column, $isExport) {
+        return sprintf('<div title class="barcode" data-barcode-symbology="%s">%s</div>', Mage::helper('meanbee_barcodeannotator/config')->getProductSkuBarcodeSymbology(), $value);
     }
 }
