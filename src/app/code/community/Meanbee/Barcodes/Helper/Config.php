9c682888cac817bda514fd5b007e8d66b3fd8f61
Ashley Wright <ashley.wright@meanbee.com>
2016-06-20 12:00:59 +0100

    Rename Meanbee_BarcodeAnnotator to Meanbee_Barcodes

--- src/app/code/community/Meanbee/Barcodes/Helper/Config.php
+++ src/app/code/community/Meanbee/Barcodes/Helper/Config.php
@@ -1,19 +1,19 @@
 <?php
-class Meanbee_BarcodeAnnotator_Helper_Config extends Mage_Core_Helper_Abstract {
-    const XML_ENABLED = 'meanbee_barcodeannotator/general/active';
-    const XML_ORDER_NUMBER_BARCODE_SYMBOLOGY = 'meanbee_barcodeannotator/general/order_number_barcode_symbology';
-    const XML_INVOICE_NUMBER_BARCODE_SYMBOLOGY = 'meanbee_barcodeannotator/general/invoice_number_barcode_symbology';
-    const XML_SHIPMENT_NUMBER_BARCODE_SYMBOLOGY = 'meanbee_barcodeannotator/general/shipment_number_barcode_symbology';
-    const XML_CREDITMEMO_NUMBER_BARCODE_SYMBOLOGY = 'meanbee_barcodeannotator/general/creditmemo_number_barcode_symbology';
-    const XML_PRODUCT_SKU_BARCODE_SYMBOLOGY = 'meanbee_barcodeannotator/general/product_sku_barcode_symbology';
+class Meanbee_Barcodes_Helper_Config extends Mage_Core_Helper_Abstract {
+    const XML_ANNOTATOR_ENABLED = 'meanbee_barcodes/annotator/active';
+    const XML_ORDER_NUMBER_BARCODE_SYMBOLOGY = 'meanbee_barcodes/annotator/order_number_barcode_symbology';
+    const XML_INVOICE_NUMBER_BARCODE_SYMBOLOGY = 'meanbee_barcodes/annotator/invoice_number_barcode_symbology';
+    const XML_SHIPMENT_NUMBER_BARCODE_SYMBOLOGY = 'meanbee_barcodes/annotator/shipment_number_barcode_symbology';
+    const XML_CREDITMEMO_NUMBER_BARCODE_SYMBOLOGY = 'meanbee_barcodes/annotator/creditmemo_number_barcode_symbology';
+    const XML_PRODUCT_SKU_BARCODE_SYMBOLOGY = 'meanbee_barcodes/annotator/product_sku_barcode_symbology';
 
     /**
      * @param null $store
      *
      * @return bool
      */
-    public function getEnabled($store = null) {
-        return Mage::getStoreConfigFlag(self::XML_ENABLED, $store);
+    public function getAnnotatorEnabled($store = null) {
+        return Mage::getStoreConfigFlag(self::XML_ANNOTATOR_ENABLED, $store);
     }
 
     /**
